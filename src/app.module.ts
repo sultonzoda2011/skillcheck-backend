9b6ab34a69afc273e1c5af21211fe988765344c2
Abdulloh <sultonzoda2011@gmail.com>
2026-03-28 08:39:29 +0500

    feat: add TestsModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TestsModule } from './tests/tests.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     PrismaModule,
     AuthModule,
+    TestsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
