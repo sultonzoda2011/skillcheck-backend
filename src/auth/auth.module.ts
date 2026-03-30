a7e68ac1ddb174d64da4e2dd7142480f1845a70c
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:20 +0500

    refactor(auth): update auth module providers and imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,16 +6,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getJwtConfig } from 'src/config/jwt.config';
 import { JwtStrategy } from 'src/strategies/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
-
+import { ProfileModule } from '../profile/profile.module';
 @Module({
   imports: [
     PassportModule,
+    ProfileModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: getJwtConfig,
       inject: [ConfigService],
     }),
   ],
+  exports: [JwtStrategy],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
