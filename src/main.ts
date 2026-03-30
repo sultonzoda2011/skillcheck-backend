b51fa2ede5386e2b6792803a314a8ddc9e8df789
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:24 +0500

    refactor(bootstrap): update application bootstrap setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 import cookieParser = require('cookie-parser');
 import { setupSwagger } from 'src/utils/swagger.util';
+import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalGuards();
   setupSwagger(app);
   await app.listen(4000);
