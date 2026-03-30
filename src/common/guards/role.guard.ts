78cad30bd1e4ed30acba126258f470644732ccbb
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:23 +0500

    feat(common): add role-based access guard

---
+++ src/common/guards/role.guard.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/generated/prisma/enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    const request = context.switchToHttp().getRequest();
+    console.log('🔍 User in RolesGuard:', request.user);
+
+    if (!requiredRoles) return true;
+
+    const user = request.user;
+
+    if (!user || !user.role || !requiredRoles.includes(user.role)) {
+      console.log('❌ Role check failed. User role:', user?.role);
+      throw new ForbiddenException(
+        'Доступ запрещён. Требуется роль администратора.',
+      );
+    }
+
+    return true;
+  }
+}
