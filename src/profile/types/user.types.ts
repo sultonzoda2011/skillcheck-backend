51af258ed95a67884b88626ebbdf862bf36ad0d2
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:26 +0500

    feat(profile): define profile user domain types

---
+++ src/profile/types/user.types.ts
@@ -0,0 +1,16 @@
+import { Prisma, User } from 'src/generated/prisma/browser';
+
+export type SafeUser = Omit<User, 'password'>;
+
+export const USER_SAFE_SELECT = {
+  id: true,
+  email: true,
+  fullName: true,
+  profilePicture: true,
+  isBlocked: false,
+  role: false,
+  createdAt: true,
+  updatedAt: true,
+  bestResult: true,
+  password: false,
+} satisfies Prisma.UserSelect;
