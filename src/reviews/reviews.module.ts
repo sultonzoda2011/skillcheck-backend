2c6d819c409d26697b8a9e842a5b83323c6445da
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 12:57:49 +0500

    refactor(reviews): wire ProfileModule dependency into reviews module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { ProfileModule } from 'src/profile/profile.module';
 
 @Module({
+  imports: [ProfileModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
