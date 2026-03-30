862a4b3e2f17d68c96f551c5130fc11976d60737
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:23 +0500

    feat(best-results): define best result domain types

---
+++ src/best-results/types/best-result.types.ts
@@ -0,0 +1,12 @@
+export type BestResult = {
+  userId: string;
+  totalScore: number;
+  bestFrontendScore: number;
+  bestBackendScore: number;
+  bestMobileScore: number;
+  frontendAchievedAt: Date | null;
+  backendAchievedAt: Date | null;
+  mobileAchievedAt: Date | null;
+};
+
+export type BestResultUpdateData = Partial<Omit<BestResult, 'userId'>>;
