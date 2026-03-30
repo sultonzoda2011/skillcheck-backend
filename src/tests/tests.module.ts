6bd54b0d9734979d4b106ac9214bd1fd2138378a
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:27 +0500

    chore(tests): remove obsolete tests module

--- src/tests/tests.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestsService } from './tests.service';
-import { TestsController } from './tests.controller';
-
-@Module({
-  providers: [TestsService],
-  controllers: [TestsController],
-})
-export class TestsModule {}
