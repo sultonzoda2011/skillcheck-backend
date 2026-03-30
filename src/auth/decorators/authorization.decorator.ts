97d76ae96ae84664da4c9ff438e016512fe51f3c
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:21 +0500

    refactor(auth): refine authorization decorator

--- src/auth/decorators/authorization.decorator.ts
+++ src/auth/decorators/authorization.decorator.ts
@@ -1,6 +1,7 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtGuard } from 'src/auth/guards/auth.guard';
 
 export function Authorization() {
-  return applyDecorators(UseGuards(JwtGuard));
+  return applyDecorators(UseGuards(JwtGuard), ApiBearerAuth());
 }
