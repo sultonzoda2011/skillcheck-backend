609fd21c6af9feb9d0b52c3b2996ac1a51abd9dc
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 12:57:48 +0500

    fix(bootstrap): handle startup promise rejections explicitly

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
+import { setupSwagger } from 'src/utils/swagger.util';
 import { AppModule } from './app.module';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import cookieParser = require('cookie-parser');
-import { setupSwagger } from 'src/utils/swagger.util';
-import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,4 +15,7 @@ async function bootstrap() {
   setupSwagger(app);
   await app.listen(4000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
