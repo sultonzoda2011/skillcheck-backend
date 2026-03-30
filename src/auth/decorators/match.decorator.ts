f5e773cc59cfab85d3d4e59da82f595f6169507c
Abdulloh <sultonzoda2011@gmail.com>
2026-03-30 11:12:21 +0500

    feat(auth): add match decorator for validation

---
+++ src/auth/decorators/match.decorator.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'Match', async: false })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const [relatedPropertyName] = args.constraints;
+    const relatedValue = (args.object as any)[relatedPropertyName];
+    return value === relatedValue;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    const [relatedPropertyName] = args.constraints;
+    return `${args.property} must match ${relatedPropertyName}`;
+  }
+}
+
+export function Match(property: string, validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
+}
