4f2ce30893fd475f4903b9bda7f1ea2d91589804
Abdulloh <sultonzoda2011@gmail.com>
2026-03-31 08:01:41 +0500

    refactor(main): update cookie-parser import and fix script path in package.json

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { setupSwagger } from './utils/swagger.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser());
 
   app.enableCors({
