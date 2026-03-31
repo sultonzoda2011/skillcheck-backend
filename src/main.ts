d6c03b2a7456f90d7b85c77956e20fb73cf905f0
Abdulloh <sultonzoda2011@gmail.com>
2026-03-31 07:40:01 +0500

    refactor(main): update cookie-parser import and enhance CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 import { setupSwagger } from 'src/utils/swagger.util';
 import { AppModule } from './app.module';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://skillcheck.kavsaracademy.tj',
+      /\.vercel\.app$/,
+    ],
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
+  });
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalGuards();
+
   setupSwagger(app);
-  await app.listen(4000);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap().catch((err) => {
   console.error(err);
