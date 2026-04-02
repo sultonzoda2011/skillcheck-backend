bed510544a9c18163cc7b6be0c2b54c54e09a4b6
Abdulloh <sultonzoda2011@gmail.com>
2026-04-02 12:12:11 +0500

    ref:link to Quard

--- src/auth/decorators/authorization.decorator.ts
+++ src/auth/decorators/authorization.decorator.ts
@@ -1,6 +1,6 @@
+import { JwtGuard } from '@/auth/guards/auth.guard';
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { JwtGuard } from 'src/auth/guards/auth.guard';
 
 export function Authorization() {
   return applyDecorators(UseGuards(JwtGuard), ApiBearerAuth());
