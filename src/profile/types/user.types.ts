77b2f7105be3e272bf5d7a1c2a1ac2d635bf4b85
Abdulloh <sultonzoda2011@gmail.com>
2026-04-02 09:05:37 +0500

    Update src/profile/types/user.types.ts

--- src/profile/types/user.types.ts
+++ src/profile/types/user.types.ts
@@ -1,4 +1,4 @@
-import { Prisma, User } from 'src/generated/prisma/browser';
+import { Prisma, User } from 'src/generated/prisma';
 
 export type SafeUser = Omit<User, 'password'>;
 
@@ -9,6 +9,19 @@ export const USER_SAFE_SELECT = {
   profilePicture: true,
   isBlocked: false,
   role: false,
+  createdAt: true,
+  updatedAt: true,
+  bestResult: true,
+  password: false,
+} satisfies Prisma.UserSelect;
+
+export const ADMIN_USER_SELECT = {
+  id: true,
+  email: true,
+  fullName: true,
+  profilePicture: true,
+  isBlocked: true,
+  role: true,
   createdAt: true,
   updatedAt: true,
   bestResult: true,
