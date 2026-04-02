a9c04e1066798f67874647a1c792d896bf387eea
Abdulloh <sultonzoda2011@gmail.com>
2026-04-02 09:56:42 +0500

    fix: replace absolute src/ imports with relative paths in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { getJwtConfig } from 'src/config/jwt.config';
-import { JwtStrategy } from 'src/strategies/jwt.strategy';
+
+import { getJwtConfig } from '../../src/config/jwt.config';
+import { JwtStrategy } from '../../src/strategies/jwt.strategy';
+
 import { MailModule } from '../mail/mail.module';
 import { ProfileModule } from '../profile/profile.module';
+
+import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 @Module({
